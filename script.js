9086438952219c6f541c83bf75af51e67e127c61
kistoo <tomio.nakamura15@gmail.com>
2022-12-01 18:05:18 -0500

    Add display library function and fix errors

--- script.js
+++ script.js
@@ -20,20 +20,87 @@ function addSongToLibrary(song){
     myLibrary.push(song);
 }
 
+function displayLibrary() {
+    myLibrary.forEach(item => {
+        const song = document.createElement('div');
+        song.className = "song";
+        const info = document.createElement('div');
+        info.className = "info";
+        const actions = document.createElement('div');
+        actions.className = "actions";
+    
+        const preview = document.createElement('img');
+        const videoID = item.youtubeLink.substring(item.youtubeLink.indexOf('=') + 1);
+        preview.src = `https://img.youtube.com/vi/${videoID}/1.jpg`;
+        preview.alt = "youtube preview";
+        
+        const name = document.createElement('h2');
+        name.textContent = `${item.name}`;
+    
+        const artist = document.createElement('h4');
+        artist.textContent = `${item.artist}`;
+    
+        const date = document.createElement('p');
+        date.textContent = `Added on ${item.addDate}`;
+    
+        const isFavourite = document.createElement('img');
+        if (item.isFavourite === true) {
+            isFavourite.src = "images/cards-heart.svg";
+        } else {
+            isFavourite.src = "images/cards-heart-outline.svg";
+        }
+        isFavourite.alt = "favourite icon";
+    
+        const link = document.createElement('a');
+        link.href = item.youtubeLink;
+        link.target = "_blank";
+        link.rel = "noreferrer noopener";
+        const playButton = document.createElement('img');
+        playButton.src = "images/play-box.svg";
+        playButton.alt = "youtube play icon";
+        link.append(playButton);
+        
+        song.appendChild(preview);
+        song.append(info);
+        info.append(name);
+        info.append(artist);
+        info.append(date);
+        song.append(actions);
+        actions.append(isFavourite);
+        actions.append(link);
+    
+        content.appendChild(song);
+        //moves add button to the bottom
+        let button = document.getElementById('modal-button');
+        if (button === null) {
+            button = document.createElement('button');
+            button.setAttribute('id','modal-button');
+            button.textContent = "+ add song"; 
+        } else {
+            button.remove();
+        }
+        content.appendChild(button);
+        button.addEventListener('click',()=>openModal());
+    });
+}
+
 function checkFields() {
     let rightFields = 0;
     inputNodeList.forEach(input => {
         if (input.id === "youtube-link") {
             if (checkLink()===true) {
                 rightFields++;
+                removeError(input);
             } else {
-                input.classList.add("error");
+                addError(input);
             }
         } else {
-            if (input.checkValidity()===true) {
+            if (input.value.length > 0) {
                 rightFields++;
+                removeError(input);
             } else {
-                input.classList.add("error");
+                addError(input);
+                input.setCustomValidity("Please fill this field");
             }
         }
     });
@@ -51,9 +118,13 @@ function checkFields() {
             }
         })
         const newSong = new Song(songData[0],songData[1],songData[2],songData[3]) 
-        console.log(newSong);
+        addSongToLibrary(newSong);
+        content.innerHTML="";
+        displayLibrary();
+        const button = document.getElementById('modal-button');
+        button.disabled = false;
+        closeModal();
     }
-
 }
 
 function checkLink() {
@@ -63,6 +134,10 @@ function checkLink() {
     } else {
         linkInput.setCustomValidity("Please put a valid link");
     }
+}
+
+function addError(item) {
+    item.classList.add("error");
 }
 
 function removeError(item) {
@@ -87,6 +162,8 @@ inputNodeList.forEach(input => {
     });
 })
 
+const content = document.getElementById('content');
+
 const linkInput = document.getElementById('youtube-link');
 linkInput.addEventListener("input",()=>removeError(linkInput));
 const submitButton = document.getElementById('submit');
@@ -101,61 +178,8 @@ window.onclick = function(event) {
     }
 }
 
+
+
 const coincidir = new Song("coincidir","macaco","https://www.youtube.com/watch?v=b3GyAtcoogc",true);
 addSongToLibrary(coincidir);
-
-myLibrary.forEach(item => {
-    const song = document.createElement('div');
-    song.className = "song";
-    const info = document.createElement('div');
-    info.className = "info";
-    const actions = document.createElement('div');
-    actions.className = "actions";
-
-    const preview = document.createElement('img');
-    const videoID = item.youtubeLink.substring(item.youtubeLink.indexOf('=') + 1);
-    preview.src = `https://img.youtube.com/vi/${videoID}/1.jpg`;
-    preview.alt = "youtube preview";
-    
-    const name = document.createElement('h2');
-    name.textContent = `${item.name}`;
-
-    const artist = document.createElement('h4');
-    artist.textContent = `${item.artist}`;
-
-    const date = document.createElement('p');
-    date.textContent = `Added on ${item.addDate}`;
-
-    const isFavourite = document.createElement('img');
-    if (item.isFavourite === true) {
-        isFavourite.src = "images/cards-heart.svg";
-    } else {
-        isFavourite.src = "images/cards-heart-outline.svg";
-    }
-    isFavourite.alt = "favourite icon";
-
-    const link = document.createElement('a');
-    link.href = item.youtubeLink;
-    link.target = "_blank";
-    link.rel = "noreferrer noopener";
-    const playButton = document.createElement('img');
-    playButton.src = "images/play-box.svg";
-    playButton.alt = "youtube play icon";
-    link.append(playButton);
-    
-    song.appendChild(preview);
-    song.append(info);
-    info.append(name);
-    info.append(artist);
-    info.append(date);
-    song.append(actions);
-    actions.append(isFavourite);
-    actions.append(link);
-
-    document.getElementById('content').appendChild(song);
-    //moves add button to the bottom
-    const button = document.getElementById('modal-button');
-    button.remove();
-    document.getElementById('content').appendChild(button);
-    button.addEventListener('click',()=>openModal());
-});
+displayLibrary();
